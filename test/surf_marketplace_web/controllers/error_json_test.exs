0590596aaf3fe137154eae7a51ec7f0dc6a54d65
Basile Nouvellet <b.nouvellet@gmail.com>
2023-04-18 09:56:16 +0100

    Run mix format

--- test/surf_marketplace_web/controllers/error_json_test.exs
+++ test/surf_marketplace_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule SurfMarketplaceWeb.ErrorJSONTest do
   use SurfMarketplaceWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert SurfMarketplaceWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert SurfMarketplaceWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
