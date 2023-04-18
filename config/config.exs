0fb494002f58ce02a0bb98180a93d39d60e784e4
Basile Nouvellet <b.nouvellet@gmail.com>
2023-04-18 15:17:19 +0100

    Update Tailwind from v3.2.7 to v3.3.1

--- config/config.exs
+++ config/config.exs
@@ -42,7 +42,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.2.7",
+  version: "3.3.1",
   default: [
     args: ~w(
       --config=tailwind.config.js
