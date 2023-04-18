21f762261a04b89bac92e6b47d349a9b197b88d3
Basile Nouvellet <b.nouvellet@gmail.com>
2023-04-18 17:44:39 +0100

    Add dummy price input in Home

--- lib/surf_marketplace_web/live/home_live.ex
+++ lib/surf_marketplace_web/live/home_live.ex
@@ -3,11 +3,24 @@ defmodule SurfMarketplaceWeb.HomeLive do
 
   def render(assigns) do
     ~H"""
-    hey there
+    <form id="price_form" phx-change="price_changed">
+      <label for="price_input" class="font-semibold mr-2">Price:</label>
+      <input id="price_input" type="number" name="price" />
+    </form>
     """
   end
 
   def mount(_params, _session, socket) do
+    socket =
+      assign(socket,
+        page_title: "Home",
+        price: nil
+      )
+
     {:ok, socket}
+  end
+
+  def handle_event("price_changed", %{"price" => price} = _args, socket) do
+    {:noreply, assign(socket, price: price)}
   end
 end
